66f91497c8ad7c7b4eac1514a4bbc908a3d9d5e4
Gustavo Werle <gustavo.werle2@gmail.com>
2024-07-30 22:38:16 -0300

    feat: add brands create and list operations

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from . import models, forms
+from django.urls import reverse_lazy
 
-# Create your views here.
+class BrandListView(ListView):
+    model = models.Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains = name)
+
+        return queryset
+    
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
